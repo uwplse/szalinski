f4f7aebe3fd0fb797371dc73bb3bfb8be0c9f6a7
Chandrakana Nandi <cnandi@cs.washington.edu>
2019-10-13 17:48:37 -0700

    handling nested loops in eval

--- src/eval.rs
+++ src/eval.rs
@@ -6,9 +6,9 @@ use std::fs::{read_dir, read_to_string};
 use smallvec::smallvec;
 
 use egg::expr::{Expr, RecExpr};
-use egg::{parse::ParsableLanguage};
+use egg::parse::ParsableLanguage;
 
-use crate::cad::{Cad};
+use crate::cad::Cad;
 use crate::cad::Variable;
 
 macro_rules! rec {
@@ -31,8 +31,8 @@ fn get_num(expr: &RecExpr<Cad>) -> f64 {
 // y=rsinϕsinθ
 // z=rcosϕ
 // https://keisan.casio.com/exec/system/1359534351
-fn to_cartesian (v: (f64, f64, f64)) -> (f64, f64, f64) {
-    fn to_rad (deg: f64) -> f64 {
+fn to_cartesian(v: (f64, f64, f64)) -> (f64, f64, f64) {
+    fn to_rad(deg: f64) -> f64 {
         deg * std::f64::consts::PI / 180.0
     }
     let r = v.0;
@@ -48,18 +48,17 @@ fn eval_fun(expr: &RecExpr<Cad>, i: usize) -> f64 {
     let expr = expr.as_ref();
     match &expr.op {
         Cad::Num(_) => get_num(&rec!(expr.op.clone())),
-        Cad::Variable(_) => i as f64,
         Cad::ListVar(_) => i as f64,
         Cad::Add => {
             let a = expr.children[0].clone();
             let b = expr.children[1].clone();
             eval_fun(&a, i) + eval_fun(&b, i)
-        },
+        }
         Cad::Mul => {
             let a = expr.children[0].clone();
             let b = expr.children[1].clone();
             eval_fun(&a, i) * eval_fun(&b, i)
-        },
+        }
         // TODO sub and div
         _ => panic!("Cannot evaluate non-arithmetic functions: {}", expr.op),
     }
@@ -68,20 +67,67 @@ fn eval_fun(expr: &RecExpr<Cad>, i: usize) -> f64 {
 fn eval_mapi(op: &Cad, expr: &RecExpr<Cad>) -> Vec<(f64, f64, f64)> {
     let mapi = expr.as_ref();
     let mut v = Vec::new();
-    let n = mapi.children[0].clone();
-    let vec = mapi.children[1].as_ref();
-    let x = vec.children[0].clone();
-    let y = vec.children[1].clone();
-    let z = vec.children[2].clone();
-    for i in 0..(get_num(&n) as usize) {
-        let (fx, fy, fz) = (eval_fun(&x, i), eval_fun(&y, i), eval_fun(&z, i));
-        match op {
-            Cad::TransPolar => {
-                let (px, py, pz) = to_cartesian ((fx, fy, fz));
-                v.push((px, py, pz));
+    if mapi.children.len() == 2 {
+        let n = mapi.children[0].clone();
+        let vec = mapi.children[1].as_ref();
+        let x = vec.children[0].clone();
+        let y = vec.children[1].clone();
+        let z = vec.children[2].clone();
+        for i in 0..(get_num(&n) as usize) {
+            let (fx, fy, fz) = (eval_fun(&x, i), eval_fun(&y, i), eval_fun(&z, i));
+            match op {
+                Cad::TransPolar => {
+                    let (px, py, pz) = to_cartesian((fx, fy, fz));
+                    v.push((px, py, pz));
+                }
+                _ => {
+                    v.push((fx, fy, fz));
+                }
             }
-            _ => {
-            v.push((fx, fy, fz));
+        }
+    } else if mapi.children.len() == 3 {
+        let nx = mapi.children[0].clone();
+        let ny = mapi.children[1].clone();
+        let vec = mapi.children[2].as_ref();
+        let x = vec.children[0].clone();
+        let y = vec.children[1].clone();
+        let z = vec.children[2].clone();
+        for i in 0..(get_num(&nx) as usize) {
+            for j in 0..(get_num(&ny) as usize) {
+                let (fx, fy, fz) = (eval_fun(&x, i), eval_fun(&y, j), eval_fun(&z, i));
+                match op {
+                    Cad::TransPolar => {
+                        let (px, py, pz) = to_cartesian((fx, fy, fz));
+                        v.push((px, py, pz));
+                    }
+                    _ => {
+                        v.push((fx, fy, fz));
+                    }
+                }
+            }
+        }
+    } else {
+        let nx = mapi.children[0].clone();
+        let ny = mapi.children[1].clone();
+        let nz = mapi.children[2].clone();
+        let vec = mapi.children[3].as_ref();
+        let x = vec.children[0].clone();
+        let y = vec.children[1].clone();
+        let z = vec.children[2].clone();
+        for i in 0..(get_num(&nx) as usize) {
+            for j in 0..(get_num(&ny) as usize) {
+                for k in 0..(get_num(&nz) as usize) {
+                    let (fx, fy, fz) = (eval_fun(&x, i), eval_fun(&y, j), eval_fun(&z, k));
+                    match op {
+                        Cad::TransPolar => {
+                            let (px, py, pz) = to_cartesian((fx, fy, fz));
+                            v.push((px, py, pz));
+                        }
+                        _ => {
+                            v.push((fx, fy, fz));
+                        }
+                    }
+                }
             }
         }
     }
@@ -96,7 +142,6 @@ pub fn cads_to_union(cs: Vec<RecExpr<Cad>>) -> RecExpr<Cad> {
     cad
 }
 
-
 pub fn eval(expr: &RecExpr<Cad>) -> RecExpr<Cad> {
     let expr = expr.as_ref();
     let arg = |i: usize| expr.children[i].clone();
@@ -106,15 +151,17 @@ pub fn eval(expr: &RecExpr<Cad>) -> RecExpr<Cad> {
         Cad::Cylinder => rec!(Cad::Cylinder),
         Cad::Hexagon => rec!(Cad::Hexagon),
         Cad::Empty => rec!(Cad::Empty),
-        Cad::Hull => rec!(Cad::Hull, arg(0)),
-        Cad::Trans => rec!(Cad::Trans, arg(0), arg(1)),
-        Cad::TransPolar => rec!(Cad::Trans, arg(0), arg(1)),
-        Cad::Scale => rec!(Cad::Scale, arg(0), arg(1)),
-        Cad::Rotate => rec!(Cad::Rotate, arg(0), arg(1)),
-        Cad::Diff => rec!(Cad::Diff, arg(0), arg(1)),
-        Cad::Inter => rec!(Cad::Inter, arg(0), arg(1)),
-        Cad::Union => rec!(Cad::Union, arg(0), arg(1)),
-        Cad::Repeat => rec!(Cad::Repeat, arg(0), arg(1)),
+        Cad::Num(f) => rec!(Cad::Num(*f)),
+        Cad::Vec => rec!(Cad::Vec, eval(&arg(0)), eval(&arg(1)), eval(&arg(2))),
+        Cad::Hull => rec!(Cad::Hull, eval(&arg(0))),
+        Cad::Trans => rec!(Cad::Trans, eval(&arg(0)), eval(&arg(1)), eval(&arg(2)), eval(&arg(3))),
+        Cad::TransPolar => rec!(Cad::Trans, eval(&arg(0)), eval(&arg(1)), eval(&arg(2)), eval(&arg(3))),
+        Cad::Scale => rec!(Cad::Scale, eval(&arg(0)), eval(&arg(1)), eval(&arg(2)), eval(&arg(3))),
+        Cad::Rotate => rec!(Cad::Rotate, eval(&arg(0)), eval(&arg(1)), eval(&arg(2)), eval(&arg(3))),
+        Cad::Diff => rec!(Cad::Diff, eval(&arg(0)), eval(&arg(1))),
+        Cad::Inter => rec!(Cad::Inter, eval(&arg(0)), eval(&arg(1))),
+        Cad::Union => rec!(Cad::Union, eval(&arg(0)), eval(&arg(1))),
+        Cad::Repeat => rec!(Cad::Repeat, eval(&arg(0)), eval(&arg(1))),
         Cad::FoldUnion => eval(&expr.children[0]),
         Cad::Map => {
             let op = &expr.children[0].as_ref().op;
@@ -127,11 +174,10 @@ pub fn eval(expr: &RecExpr<Cad>) -> RecExpr<Cad> {
                 let vx = rec!(Cad::Num(fs[i].0.into()));
                 let vy = rec!(Cad::Num(fs[i].1.into()));
                 let vz = rec!(Cad::Num(fs[i].2.into()));
-                let a = rec!(Cad::Vec, vx, vy, vz);
-                v.push(rec!(op.clone(), a, c.clone()));
+                v.push(rec!(op.clone(), vx, vy, vz, c.clone()));
             }
             cads_to_union(v)
-        },
+        }
         _ => RecExpr::from(expr.clone()),
     }
 }
@@ -145,23 +191,23 @@ impl<'a> fmt::Display for Scad<'a> {
         let child = |i: usize| Scad(&expr.children[i]);
         match expr.op {
             Cad::Num(float) => write!(f, "{}", float),
+            Cad::Vec => write!(f, "[{}, {}, {}]", child(0), child(1), child(2)),
+            Cad::Add => write!(f, "{} + {}", child(0), child(1)),
+            Cad::Sub => write!(f, "{} - {}", child(0), child(1)),
+            Cad::Mul => write!(f, "{} * {}", child(0), child(1)),
+            Cad::Div => write!(f, "{} / {}", child(0), child(1)),
             Cad::Empty => write!(f, "sphere(r=0);"),
             Cad::Unit => write!(f, "cube();"),
             Cad::Sphere => write!(f, "sphere($fn = 50);"),
             Cad::Cylinder => write!(f, "cylinder($fn = 50);"),
             Cad::Hexagon => write!(f, "cylinder();"),
             Cad::Hull => write!(f, "hull() {{ {} }}", child(0)),
-            Cad::Trans => write!(f, "translate({}) {}", child(0), child(1)),
-            Cad::Scale => write!(f, "scale ({}) {}", child(0), child(1)),
-            Cad::Rotate => write!(f, "rotate ({}) {}", child(0), child(1)),
+            Cad::Trans => write!(f, "translate ([{}, {}, {}]) {}", child(0), child(1), child(2), child(3)),
+            Cad::Scale => write!(f, "scale ([{}, {}, {}]) {}", child(0), child(1), child(2), child(3)),
+            Cad::Rotate => write!(f, "rotate ([{}, {}, {}]) {}", child(0), child(1), child(2), child(3)),
             Cad::Union => write!(f, "union () {{ {} {} }}", child(0), child(1)),
             Cad::Inter => write!(f, "intersection () {{ {} {} }}", child(0), child(1)),
             Cad::Diff => write!(f, "difference () {{ {} {} }}", child(0), child(1)),
-            Cad::Vec => write!(f, "[{}, {}, {}]", child(0), child(1), child(2)),
-            Cad::Add => write!(f, "{} + {}", child(0), child(1)),
-            Cad::Sub => write!(f, "{} - {}", child(0), child(1)),
-            Cad::Mul => write!(f, "{} * {}", child(0), child(1)),
-            Cad::Div => write!(f, "{} / {}", child(0), child(1)),
             _ => write!(f, "TODO"),
         }
     }
@@ -178,10 +224,11 @@ macro_rules! test_eval {
             let start = Cad::parse_expr(&input).unwrap();
             let res = format!("{}", Scad(&start)).trim().to_string();
             let actual = res.trim();
+            println!("{}", actual);
             if outfile.contains("scad-output/") && output.is_ok() {
                 let expected = &output.unwrap().trim().to_string();
                 if actual != expected {
-                    let diff = colored_diff::PrettyDifference {expected, actual};
+                    let diff = colored_diff::PrettyDifference { expected, actual };
                     panic!("Didn't match expected. {}", diff)
                 }
                 assert_eq!(actual, expected);
@@ -192,9 +239,9 @@ macro_rules! test_eval {
     };
 }
 
-//test_eval! {box_flat, "cads/pldi2020-eval/expected/box_flat.csexp", "" }
-test_eval! {flower, "cads/pldi2020-eval/expected/flower.csexp" }
-//test_eval! {cnc, "cads/pldi2020-eval/expected/cnc_endmill_nohull.csexp" }
+test_eval! {eval_box_flat, "cads/pldi2020-eval/expected/box_flat.csexp" }
+test_eval! {eval_flower, "cads/pldi2020-eval/expected/flower.csexp" }
+test_eval! {eval_cnc, "cads/pldi2020-eval/expected/cnc_endmill_nohull.csexp" }
 
 #[test]
 fn eval_prim() {
@@ -204,7 +251,7 @@ fn eval_prim() {
 }
 
 #[test]
-fn eval_affine() {
+fn eval_affine1() {
     let input = rec!(
         Cad::Trans,
         rec!(
@@ -218,6 +265,25 @@ fn eval_affine() {
     let output = eval(&input);
     assert_eq!(output, input);
 }
+
+#[test]
+fn eval_affine2() {
+    let input = rec!(
+        Cad::Scale,
+        rec!(
+            Cad::Vec,
+            rec!(Cad::Num(2.into())),
+            rec!(Cad::Num(3.into())),
+            rec!(Cad::Num(5.into()))
+        ),
+        rec!(Cad::Unit)
+    );
+    let output = eval(&input);
+    let f = format!("{}", Scad(&output));
+    println!("{}", f);
+    assert_eq!(output, input);
+}
+
 
 #[test]
 fn scad_foldunion() {
