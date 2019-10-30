a837a0226414885435f0c09354db290b244a9082
Max Willsey <me@mwillsey.com>
2019-10-30 15:50:34 -0700

    Implement (but don't enable) quantizing

--- src/num.rs
+++ src/num.rs
@@ -2,9 +2,15 @@ use std::fmt;
 use std::str::FromStr;
 
 #[derive(PartialOrd, Ord, PartialEq, Eq, Hash, Default, Clone, Copy)]
-pub struct Num(pub ordered_float::NotNan<f64>);
+// pub struct Num(ordered_float::NotNan<f64>);
+pub struct Num(u64);
 
 const EPSILON: f64 = 0.1;
+
+const DEFAULT_MANTISSA_BITS: usize = 52;
+const MANTISSA_BITS: usize = 52;
+
+// const ROUND_RELATIVE: f64 = 0.01;
 
 pub fn num(n: impl Into<Num>) -> Num {
     n.into()
@@ -12,7 +18,8 @@ pub fn num(n: impl Into<Num>) -> Num {
 
 impl Num {
     pub fn to_f64(self) -> f64 {
-        self.0.into()
+        // self.0.into_inner()
+        f64::from_bits(self.0)
     }
 
     pub fn is_close(self, other: f64) -> bool {
@@ -24,7 +31,12 @@ impl Num {
 
 impl From<f64> for Num {
     fn from(f: f64) -> Num {
-        Num(f.into())
+        let d = DEFAULT_MANTISSA_BITS - MANTISSA_BITS;
+        let mask: u64 = !0 << d;
+        Num(f.to_bits() & mask)
+        // let small = f * ROUND_RELATIVE;
+        // let big = small.round() / ROUND_RELATIVE;
+        // Num(big.into())
     }
 }
 
@@ -47,7 +59,8 @@ impl From<i32> for Num {
 impl FromStr for Num {
     type Err = ordered_float::ParseNotNanError<std::num::ParseFloatError>;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        s.parse().map(Num)
+        let f: ordered_float::NotNan<f64> = s.parse()?;
+        Ok(f.into_inner().into())
     }
 }
 
