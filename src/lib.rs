da1cd2e5b12ea3a92576295702b7d0cbf09333f8
Max Willsey <me@mwillsey.com>
2019-11-12 09:50:36 -0800

    Put parameters in an environment file

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,23 @@
+
+#[macro_export]
+macro_rules! sz_param {
+    ($name:ident : $ty:ty) => {
+        static $name: once_cell::sync::Lazy<$ty> = once_cell::sync::Lazy::new(|| {
+            let var = std::concat!("SZ_", stringify!($name));
+            match std::env::var(var) {
+                Ok(s) => match s.parse() {
+                    Ok(t) => {
+                        log::info!("Parsed {}={}", var, t);
+                        t
+                    }
+                    Err(err) => panic!("Couldn't parse '{}={}': {}", var, s, err),
+                }
+                Err(err) => panic!("Couldn't read {}: {}", var, err)
+            }
+        });
+    }
+}
+
 pub mod cad;
 pub mod rules;
 
