fb355d1effe0d4e10b136b15fd81e0783fbcaf64
Max Willsey <me@mwillsey.com>
2019-11-20 11:17:31 -0800

    Improve param macros

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,16 @@
 #[macro_export]
 macro_rules! sz_param {
     ($name:ident : $ty:ty) => {
+        $crate::sz_param!($name : $ty = |x: $ty| x);
+    };
+    ($name:ident : $ty:ty = |$arg:ident : $parse_ty:ty| $($body:tt)+) => {
         static $name: once_cell::sync::Lazy<$ty> = once_cell::sync::Lazy::new(|| {
             let var = std::concat!("SZ_", stringify!($name));
             match std::env::var(var) {
-                Ok(s) => match s.parse() {
+                Ok(s) => match s.parse::<$parse_ty>() {
                     Ok(t) => {
                         log::info!("Parsed {}={}", var, t);
-                        t
+                        (|$arg: $parse_ty| $($body)+)(t)
                     }
                     Err(err) => panic!("Couldn't parse '{}={}': {}", var, s, err),
                 }
