ab13c101a4cbd75ea8b420d1509c1bcda30780ce
Anjali Pal <anjalip@cs.washington.edu>
2023-06-07 11:29:54 -0700

    pipe through from textarea to lib

--- wasm-sz/site/src/CaddyEditor.jsx
+++ wasm-sz/site/src/CaddyEditor.jsx
@@ -1,53 +1,25 @@
-import React from "react";
-
-const caddyInput = `(Fold
-  Diff
-  (Map2
-    Trans
-    (MapI
-      4
-      (Vec3
-        (+ (* (* i i) -5) (* i 15))
-        (+ (* (* i i) -4.999949999999998) (* i 14.999849999999995))
-        0))
-    (List
-      (Cube (Vec3 120 95 2.5) false)
-      (Cube (Vec3 100 75 2.5) false)
-      (Cube (Vec3 100 75 1) false)
-      (Fold
-        Union
-        (Map2
-          Trans
-          (List
-            (Vec3 77.6 35.975 0)
-            (Vec3 44.300000000000004 35.975 0)
-            (Vec3 10 35.975 0)
-            (Vec3 44.300000000000004 10 0)
-            (Vec3 77.6 10 0)
-            (Vec3 10 10 0)
-            (Vec3 44.300000000000004 60.94999999999999 0)
-            (Vec3 10 60.94999999999999 0)
-            (Vec3 77.6 60.94999999999999 0))
-          (Map2
-            Scale
-            (List
-              (Vec3 32.4 23.975 1)
-              (Vec3 32.3 23.975 1)
-              (Vec3 33.3 23.975 1)
-              (Vec3 32.3 24.975 1)
-              (Vec3 32.4 24.975 1)
-              (Vec3 33.3 24.975 1)
-              (Vec3 32.3 24.05 1)
-              (Vec3 33.3 24.05 1)
-              (Vec3 32.4 24.05 1))
-            (Repeat 9 (Cube (Vec3 1 1 1) false))))))))`;
+import React, { useState } from "react";
 
 export default function CaddyEditor(props) {
+  const { caddyToCsg } = props;
+  const [caddy, setCaddy] = useState("");
+
+  function onChange(e) {
+    setCaddy(e.target.value);
+  }
+
+  function compile() {
+    if (!caddyToCsg) {
+      return;
+    }
+    console.log(caddyToCsg(caddy));
+  }
+
   return (
     <div className="column">
       <h2>Caddy</h2>
-      <button className="hidden">hide me</button>
-      <textarea value={caddyInput}></textarea>
+      <button onClick={compile}>Compile to CSG</button>
+      <textarea value={caddy} onChange={onChange}></textarea>
     </div>
   );
 }
