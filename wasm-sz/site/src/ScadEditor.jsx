8471aabf9605ad26619ed14f434b5068d493dee9
Anjali Pal <anjalip@cs.washington.edu>
2023-06-07 11:35:32 -0700

    full render from caddy

--- wasm-sz/site/src/ScadEditor.jsx
+++ wasm-sz/site/src/ScadEditor.jsx
@@ -1,26 +1,14 @@
 import React from "react";
-import { inputs } from "./inputs.js";
 
 export default function ScadEditor(props) {
-  const { onSelect, designName } = props;
-
-  const scad = inputs[designName];
-  console.log(designName);
-  console.log(scad);
+  const { scad } = props;
 
   return (
     <div className="column">
       <h2>ScadEditor</h2>
       <div>
-        <select onChange={onSelect} name="select" id="select">
-          <option value=""></option>
-          {Object.keys(inputs).map((i) => (
-            <option key={i} value={i}>
-              {i}
-            </option>
-          ))}
-        </select>
-        <textarea value={scad}></textarea>
+        <button className="hidden">hide me</button>
+        <textarea value={scad} readOnly></textarea>
       </div>
     </div>
   );
