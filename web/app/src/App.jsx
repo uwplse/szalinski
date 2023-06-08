fdfe31011f4341718d3409bf818bd9ac12af15ec
Anjali Pal <anjalip@cs.washington.edu>
2023-06-08 14:32:08 -0700

    checkpoint

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -6,16 +6,26 @@ import { cardFramer } from "./cardFramer";
 
 export default function App(props) {
   const { wasm } = props;
-  const [renderCode, setRenderCode] = useState("");
+  const [csg, setCsg] = useState(cardFramer);
+  const [caddy, setCaddy] = useState("");
+
+  const runSzalinski = (csgCode) => {
+    try {
+      return wasm.run_szalinski(csgCode);
+    } catch (e) {
+      console.warn(e);
+    }
+  };
+  const onRunClick = () => {
+    setCaddy(runSzalinski(csg) || "");
+  };
 
   return (
     <div className="row">
-      <CodeEditor />
-      <CodeEditor
-        initialCode={cardFramer}
-        render={(caddy) => setRenderCode(caddy)}
-      />
-      <Renderer caddyToScad={wasm.caddy_to_scad} caddy={renderCode} />
+      <CodeEditor code={csg} setCode={setCsg} />
+      <button onClick={onRunClick}>Run Szalinski</button>
+      <CodeEditor code={caddy} setCode={setCaddy} readOnly />
+      <Renderer caddyToScad={wasm.caddy_to_scad} caddy={caddy} />
     </div>
   );
 }
--- web/src/lib.rs
+++ web/src/lib.rs
@@ -1,14 +1,68 @@
-use egg::RecExpr;
-use szalinski_egg::{cad::Cad, eval::Scad};
 use wasm_bindgen::prelude::wasm_bindgen;
-
-#[wasm_bindgen]
-pub fn greet(name: &str) -> String {
-    format!("Hi, {}!", name)
-}
 
 #[wasm_bindgen]
 pub fn caddy_to_scad(_s: String) -> String {
     // TODO: parse Caddy to SCAD
-    "cube([100, 200, 300], center=true);".to_string()
+    "difference () {
+  translate ([0, 0, 0]) cube([120, 95, 2.5], center=false);
+  translate ([10, 9.999899999999997, 0]) cube([100, 75, 2.5], center=false);
+  translate ([10, 9.999899999999997, 0]) cube([100, 75, 1], center=false);
+  translate ([0, 0, 0]) union () {
+  translate ([77.6, 35.975, 0]) scale ([32.4, 23.975, 1]) cube([1, 1, 1], center=false);
+  translate ([44.300000000000004, 35.975, 0]) scale ([32.3, 23.975, 1]) cube([1, 1, 1], center=false);
+  translate ([10, 35.975, 0]) scale ([33.3, 23.975, 1]) cube([1, 1, 1], center=false);
+  translate ([44.300000000000004, 10, 0]) scale ([32.3, 24.975, 1]) cube([1, 1, 1], center=false);
+  translate ([77.6, 10, 0]) scale ([32.4, 24.975, 1]) cube([1, 1, 1], center=false);
+  translate ([10, 10, 0]) scale ([33.3, 24.975, 1]) cube([1, 1, 1], center=false);
+  translate ([44.300000000000004, 60.94999999999999, 0]) scale ([32.3, 24.05, 1]) cube([1, 1, 1], center=false);
+  translate ([10, 60.94999999999999, 0]) scale ([33.3, 24.05, 1]) cube([1, 1, 1], center=false);
+  translate ([77.6, 60.94999999999999, 0]) scale ([32.4, 24.05, 1]) cube([1, 1, 1], center=false);
+}}".to_string()
+}
+
+#[wasm_bindgen]
+pub fn run_szalinski(_s: String) -> String {
+    // TODO: run Szalinski to generate Caddy
+    "(Fold
+  Diff
+  (Map2
+    Trans
+    (MapI
+      4
+      (Vec3
+        (+ (* (* i i) -5) (* i 15))
+        (+ (* (* i i) -4.999949999999998) (* i 14.999849999999995))
+        0))
+    (List
+      (Cube (Vec3 120 95 2.5) false)
+      (Cube (Vec3 100 75 2.5) false)
+      (Cube (Vec3 100 75 1) false)
+      (Fold
+        Union
+        (Map2
+          Trans
+          (List
+            (Vec3 77.6 35.975 0)
+            (Vec3 44.300000000000004 35.975 0)
+            (Vec3 10 35.975 0)
+            (Vec3 44.300000000000004 10 0)
+            (Vec3 77.6 10 0)
+            (Vec3 10 10 0)
+            (Vec3 44.300000000000004 60.94999999999999 0)
+            (Vec3 10 60.94999999999999 0)
+            (Vec3 77.6 60.94999999999999 0))
+          (Map2
+            Scale
+            (List
+              (Vec3 32.4 23.975 1)
+              (Vec3 32.3 23.975 1)
+              (Vec3 33.3 23.975 1)
+              (Vec3 32.3 24.975 1)
+              (Vec3 32.4 24.975 1)
+              (Vec3 33.3 24.975 1)
+              (Vec3 32.3 24.05 1)
+              (Vec3 33.3 24.05 1)
+              (Vec3 32.4 24.05 1))
+            (Repeat 9 (Cube (Vec3 1 1 1) false))))))))"
+        .to_string()
 }
