2c0b58756622dab82f6412a1492504cb72f6dbd6
Anjali Pal <anjalip@cs.washington.edu>
2023-06-08 14:56:49 -0700

    upload CSG from file

--- web/app/src/CodeEditor.jsx
+++ web/app/src/CodeEditor.jsx
@@ -2,10 +2,26 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
 export default function CodeEditor(props) {
-  let { code, setCode, readOnly } = props;
+  let { allowImport, code, setCode, readOnly } = props;
+
+  function onClickImport() {
+    let input = document.createElement("input");
+    input.type = "file";
+    input.onchange = async () => {
+      let files = Array.from(input.files);
+      if (files[0]) {
+        const text = await files[0].text();
+        if (text) {
+          setCode(text);
+        }
+      }
+    };
+    input.click();
+  }
 
   return (
     <div className="column">
+      {allowImport && <button onClick={onClickImport}>Import from File</button>}
       <MonacoEditor
         value={code}
         height={"95%"}
