f5e540e24217253eb61f65a17141504950fb4e40
Anjali Pal <anjalip@cs.washington.edu>
2023-06-08 10:09:38 -0700

    copy changes from https://github.com/yihozhang/szalinski/tree/cse556

--- src/bin/optimize.rs
+++ src/bin/optimize.rs
@@ -4,8 +4,10 @@ use log::*;
 use serde::Serialize;
 
 use egg::*;
-use szalinski_egg::cad::{Cad, Cost, CostFn, Meta};
-use szalinski_egg::eval::{remove_empty, Scad};
+use std::default::Default;
+use szalinski_egg::cad::{Cad, Cost, CostFn, MetaAnalysis, Rewrite};
+use szalinski_egg::eval::remove_empty;
+use szalinski_egg::rules::reroll;
 use szalinski_egg::sz_param;
 
 // #[derive(Debug, Serialize)]
@@ -255,40 +257,59 @@ pub struct RunResult {
 //     }
 // }
 
-fn ast_size(e: &RecExpr<Cad>) -> usize {
-    let e = e.as_ref();
-    let sum_children: usize = e.children.iter().map(ast_size).sum();
-    match e.op {
-        Cad::Vec3 => 1,
+fn ast_size_impl(expr: &RecExpr<Cad>, id: Id) -> usize {
+    let e = &expr[id];
+    let sum_children: usize = e.children().iter().map(|e| ast_size_impl(expr, *e)).sum();
+    match e {
+        Cad::Vec3(_) => 1,
         _ => 1 + sum_children,
     }
 }
 
-fn ast_depth(e: &RecExpr<Cad>) -> usize {
-    let e = e.as_ref();
-    let max_children = e.children.iter().map(ast_depth).max().unwrap_or(0);
-    match e.op {
-        Cad::Vec3 => 1,
+fn ast_size(e: &RecExpr<Cad>) -> usize {
+    ast_size_impl(e, (e.as_ref().len() - 1).into())
+}
+
+fn ast_depth_impl(expr: &RecExpr<Cad>, id: Id) -> usize {
+    let e = &expr[id];
+    let max_children = e
+        .children()
+        .iter()
+        .map(|e| ast_depth_impl(expr, *e))
+        .max()
+        .unwrap_or(0);
+    match e {
+        Cad::Vec3(_) => 1,
         _ => 1 + max_children,
     }
 }
 
-fn n_mapis(e: &RecExpr<Cad>) -> usize {
-    let e = e.as_ref();
-    let sum_children: usize = e.children.iter().map(n_mapis).sum();
+fn ast_depth(e: &RecExpr<Cad>) -> usize {
+    ast_depth_impl(e, (e.as_ref().len() - 1).into())
+}
+
+fn n_mapis_impl(expr: &RecExpr<Cad>, id: Id) -> usize {
+    let e = &expr[id];
+    let sum_children: usize = e.children().iter().map(|e| n_mapis_impl(expr, *e)).sum();
     sum_children
-        + match e.op {
-            Cad::MapI => 1,
+        + match e {
+            Cad::MapI(_) => 1,
             _ => 0,
         }
 }
+fn n_mapis(e: &RecExpr<Cad>) -> usize {
+    n_mapis_impl(e, (e.as_ref().len() - 1).into())
+}
 
-fn depth_under_mapis(expr: &RecExpr<Cad>) -> usize {
-    let e = expr.as_ref();
-    match e.op {
-        Cad::MapI => ast_depth(expr) - 1,
-        _ => e.children.iter().map(n_mapis).sum(),
+fn depth_under_mapis(e: &RecExpr<Cad>) -> usize {
+    fn depth_under_mapis_impl(expr: &RecExpr<Cad>, id: Id) -> usize {
+        let e = &expr[id];
+        match e {
+            Cad::MapI(_) => ast_depth_impl(expr, id) - 1,
+            _ => e.children().iter().map(|e| n_mapis_impl(expr, *e)).sum(),
+        }
     }
+    depth_under_mapis_impl(e, (e.as_ref().len() - 1).into())
 }
 
 #[derive(Serialize)]
@@ -296,7 +317,7 @@ pub struct MyIterData {
     best_cost: Cost,
 }
 
-impl IterationData<Cad, Meta> for MyIterData {
+impl IterationData<Cad, MetaAnalysis> for MyIterData {
     fn make(runner: &MyRunner) -> Self {
         let root = runner.roots[0];
         let best_cost = Extractor::new(&runner.egraph, CostFn).find_best(root).0;
@@ -304,9 +325,9 @@ impl IterationData<Cad, Meta> for MyIterData {
     }
 }
 
-type MyRunner = egg::Runner<Cad, Meta, MyIterData>;
+type MyRunner = egg::Runner<Cad, MetaAnalysis, MyIterData>;
 
-sz_param!(PRE_EXTRACT: bool);
+sz_param!(PRE_EXTRACT: bool = true);
 
 fn main() {
     let _ = env_logger::builder().is_test(false).try_init();
@@ -316,17 +337,25 @@ fn main() {
     }
     let input = std::fs::read_to_string(&args[1]).expect("failed to read input");
 
-    sz_param!(ITERATIONS: usize);
-    sz_param!(NODE_LIMIT: usize);
-    sz_param!(TIMEOUT: f64);
+    sz_param!(ITERATIONS: usize = 300);
+    sz_param!(NODE_LIMIT: usize = 3000000);
+    sz_param!(TIMEOUT: f64 = 10.0);
 
-    let initial_expr = input.parse().expect("Couldn't parse input");
-    let initial_expr = remove_empty(&initial_expr).expect("input was empty");
+    let initial_expr: RecExpr<_> = input.parse().expect("Couldn't parse input");
+
+    // remove empty
+    let n = (initial_expr.as_ref().len() - 1).into();
+    let mut out = RecExpr::from(vec![]);
+    remove_empty(&initial_expr, n, &mut out).expect("input was empty");
+    let initial_expr = out;
+    // yz: i want to write this
+    // initial_expr.compact();
+
     let initial_cost = CostFn.cost_rec(&initial_expr);
 
     let initial_expr = if *PRE_EXTRACT {
         let pre_rules = szalinski_egg::rules::pre_rules();
-        let runner = MyRunner::default()
+        let runner = MyRunner::new(MetaAnalysis::default())
             .with_iter_limit(*ITERATIONS)
             .with_node_limit(*NODE_LIMIT)
             .with_time_limit(Duration::from_secs_f64(1.0))
@@ -340,17 +369,21 @@ fn main() {
     };
 
     let rules = szalinski_egg::rules::rules();
-    let runner = MyRunner::default()
+    let mut runner = MyRunner::new(MetaAnalysis::default())
         .with_iter_limit(*ITERATIONS)
         .with_node_limit(*NODE_LIMIT)
         .with_time_limit(Duration::from_secs_f64(*TIMEOUT))
         .with_scheduler(
             BackoffScheduler::default()
                 .with_ban_length(5)
-                .with_initial_match_limit(1_000)
+                .with_initial_match_limit(1_000_00),
         )
         .with_expr(&initial_expr)
         .run(&rules);
+
+    runner.print_report();
+    let runner = reroll_and_run(runner, &rules);
+    let runner = reroll_and_run(runner, &rules);
 
     info!(
         "Stopping after {} iters: {:?}",
@@ -372,7 +405,8 @@ fn main() {
         final_cost: best.0,
         final_expr: best.1.pretty(80),
         extract_time,
-        final_scad: format!("{}", Scad(&best.1)),
+        final_scad: "".into(),
+        // final_scad: format!("{}", Scad(&best.1)),
         stop_reason: runner.stop_reason.unwrap(),
         ast_size: ast_size(&best.1),
         ast_depth: ast_depth(&best.1),
@@ -382,4 +416,21 @@ fn main() {
 
     let out_file = std::fs::File::create(&args[2]).expect("failed to open output");
     serde_json::to_writer_pretty(out_file, &report).unwrap();
+    // let dot = runner.egraph.dot();
+    // dot.to_pdf("out.pdf").unwrap();
+}
+
+pub fn reroll_and_run(
+    mut runner: egg::Runner<Cad, MetaAnalysis, MyIterData>,
+    rules: &Vec<Rewrite>,
+) -> egg::Runner<Cad, MetaAnalysis, MyIterData> {
+    let egraph = &mut runner.egraph;
+    reroll(egraph);
+
+    runner.stop_reason = None;
+    let new_limit = runner.iterations.len() + 2;
+    runner
+        .with_scheduler(SimpleScheduler)
+        .with_iter_limit(new_limit)
+        .run(rules)
 }
