3e5d7bd63e6d463d2c6e25a7f2c5414f5f802582
Yihong Zhang <yz489@cs.washington.edu>
2023-07-10 10:56:30 -0700

    let cost function consider nums as well

--- src/cad.rs
+++ src/cad.rs
@@ -272,7 +272,7 @@ impl egg::CostFunction<Cad> for CostFn {
         let cost = match enode.op {
             Num(n) => {
                 let s = format!("{}", n);
-                0.000001 * s.len() as Cost
+                1. + (0.000001 * s.len() as Cost)
             }
             Bool(_) | ListVar(_) => SMALL,
             Add | Sub | Mul | Div => SMALL,
