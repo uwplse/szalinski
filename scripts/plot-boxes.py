69b8301a710d720ef1cc472dde8913b00e9a3ad1
Yihong Zhang <yz489@cs.washington.edu>
2023-07-12 15:24:07 -0700

    update scripts

--- scripts/plot-boxes.py
+++ scripts/plot-boxes.py
@@ -11,81 +11,82 @@ import util
 
 import json
 
+
 def gray(i):
-    i = i ** 0.5
+    i = i**0.5
     return (i, i, i)
 
+
 def improvement_before(j, secs):
-    init = j['initial_cost']
+    init = j["initial_cost"]
     cost = init
     time = 0
-    for it in j['iterations']:
-        time += it['search_time'] + it['apply_time'] + it['rebuild_time']
+    for it in j["iterations"]:
+        time += it["search_time"] + it["apply_time"] + it["rebuild_time"]
         if time > secs:
             return cost / init
-        cost = it['data']['best_cost']
-    return j['final_cost'] / init
+        cost = it["data"]["best_cost"]
+    return j["final_cost"] / init
 
-names = {
-    'Slightly Perturbed': '/**/*.normal.json',
-    'Slightly Perturbed\n1 second timeout': '/**/*.normal.json',
-    'Perturbed': '/**/*.perturb.json',
-    'Perturbed\nNo InvTrans': '/**/*.perturb-noinv.json',
-    'Perturbed\nNo CAD': '/**/*.perturb-nocad.json',
-}
 
-all_data = {}
+if __name__ == "__main__":
+    names = {
+        "Slightly Perturbed": "/**/*.normal.json",
+        # 'Slightly Perturbed\n1 second timeout': '/**/*.normal.json',
+        "Perturbed": "/**/*.perturb.json",
+        # 'Perturbed\nNo InvTrans': '/**/*.perturb-noinv.json',
+        # 'Perturbed\nNo CAD': '/**/*.perturb-nocad.json',
+    }
 
-with multiprocessing.Pool() as pool:
-    for name, pattern in names.items():
-        files = glob.glob('out/thingiverse/' + pattern, recursive=True)
-        all_data[name] = pool.map(util.load_json, files)
+    all_data = {}
 
-for name, data in all_data.items():
-    print('Found', len(data), 'for', name.replace('\n', ' '))
+    with multiprocessing.Pool() as pool:
+        for name, pattern in names.items():
+            files = glob.glob("out/thingiverse/" + pattern, recursive=True)
+            all_data[name] = pool.map(util.load_json, files)
 
-plt.rc('xtick', labelsize=9)
-plt.rc('ytick', labelsize=9)
-fig, axes = plt.subplots(ncols=len(names), sharey=True, figsize=(14, 5))
-fig.subplots_adjust(wspace=0.1)
+    for name, data in all_data.items():
+        print("Found", len(data), "for", name.replace("\n", " "))
 
-SIZES=[0, 30, 100, 300]
+    plt.rc("xtick", labelsize=9)
+    plt.rc("ytick", labelsize=9)
+    fig, axes = plt.subplots(ncols=len(names), sharey=True, figsize=(14, 5))
+    fig.subplots_adjust(wspace=0.1)
 
-for ax, (name, data) in zip(axes, all_data.items()):
-    print(len(data))
-    xs = []
-    for i, lower in enumerate(SIZES):
-        upper = SIZES[i + 1] if i + 1 < len(SIZES) else float('inf')
-        def imp(j):
-            if '1 second' in name:
-                return improvement_before(j, 1)
-            else:
-                return j['improvement']
+    SIZES = [0, 30, 100, 300]
 
-        x = [
-            100 - imp(j) * 100
-            for j in data
-            if lower <= j['initial_cost'] < upper
-        ]
-        print('{} n{} {} {}'.format(name, len(x), lower, upper))
-        xs.append(x)
-    # ax.violinplot(xs)
-    ax.boxplot(xs, showfliers=False)
-    ax.xaxis.label.set_size(12)
-    ax.yaxis.label.set_size(12)
-    ax.set(xticklabels=['Tiny', 'Small', 'Med', 'Large'], xlabel=name)
+    for ax, (name, data) in zip(axes, all_data.items()):
+        print(len(data))
+        xs = []
+        for i, lower in enumerate(SIZES):
+            upper = SIZES[i + 1] if i + 1 < len(SIZES) else float("inf")
 
-gap = 1
-positions = [
-    (i * len(names)) + i * gap + j
-    for i in range(len(SIZES))
-    for j in range(len(names))
-]
-print(positions)
+            def imp(j):
+                if "1 second" in name:
+                    return improvement_before(j, 1)
+                else:
+                    return j["improvement"]
 
-axes[0].set(ylabel='% shrunk (higher is better)')
+            x = [100 - imp(j) * 100 for j in data if lower <= j["initial_cost"] < upper]
+            print("{} n{} {} {}".format(name, len(x), lower, upper))
+            xs.append(x)
+        # ax.violinplot(xs)
+        ax.boxplot(xs, showfliers=False)
+        ax.xaxis.label.set_size(12)
+        ax.yaxis.label.set_size(12)
+        ax.set(xticklabels=["Tiny", "Small", "Med", "Large"], xlabel=name)
 
-filename = sys.argv[1]
-plt.savefig(filename, bbox_inches='tight', dpi=500)
-print('Saved plot to', filename)
-# plt.show()
+    gap = 1
+    positions = [
+        (i * len(names)) + i * gap + j
+        for i in range(len(SIZES))
+        for j in range(len(names))
+    ]
+    print(positions)
+
+    axes[0].set(ylabel="% shrunk (higher is better)")
+
+    filename = sys.argv[1]
+    plt.savefig(filename, bbox_inches="tight", dpi=500)
+    print("Saved plot to", filename)
+    # plt.show()
